d1a909264477c7d5354a139b0f5c039be0c65692
Chris Gerth <chrisgerth010592@gmail.com>
2018-06-14 18:21:43 -0500

    Initial dev for arudino jevois pc bridge

---
+++ serial_bridge/bridge.ps1
@@ -0,0 +1,37 @@
+$jevois_port_name  = COM3
+$arduino_port_name = COM4
+
+$jevois_port = new-Object System.IO.Ports.SerialPort $jevois_port_name,115200,None,8,one
+$arduino_port = new-Object System.IO.Ports.SerialPort $arduino_port_name,115200,None,8,one
+
+$jevois_port.ReadTimeout = 50
+$arduino_port.ReadTimeout = 50
+
+
+echo "[bridge] Starting serial listening. Press Q to quit."
+
+
+while($true){
+
+    try{
+        $jevois_output  = $jevois_port.ReadLine()
+        $arduino_output = $arduino_port.ReadLine()
+        echo $jevois_output
+        echo $arduino_port
+        $jevois_port.write($arduino_output)
+        $arduino_port.write($jevois_output)
+        
+    } catch {
+       # Start-Sleep -m 50
+    }
+
+    # Check if Q was pressed and quit if so.
+    if ([console]::KeyAvailable) {
+        $key = [system.console]::readkey($true)
+        if (($key.key -eq "Q")) {
+            echo "Quitting, user pressed Q..."
+            break
+        }
+    }
+    
+}
