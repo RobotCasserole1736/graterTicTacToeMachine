e626fa30f39701b8a1df275058d2df76999d30c2
ADA <ada@robotcasserole.org>
2018-06-14 19:03:56 -0500

    debug more

--- serial_bridge/bridge.ps1
+++ serial_bridge/bridge.ps1
@@ -1,11 +1,14 @@
-$jevois_port_name  = COM4
-$arduino_port_name = COM3
+$jevois_port_name  = "COM6"
+$arduino_port_name = "COM4"
 
 $jevois_port = new-Object System.IO.Ports.SerialPort $jevois_port_name,115200,None,8,one
 $arduino_port = new-Object System.IO.Ports.SerialPort $arduino_port_name,115200,None,8,one
 
-$jevois_port.ReadTimeout = 50
-$arduino_port.ReadTimeout = 50
+$jevois_port.Open()
+$arduino_port.Open()
+
+$jevois_port.ReadTimeout = 200
+$arduino_port.ReadTimeout = 200
 
 echo "[bridge] Starting serial listening. Press Q to quit."
 
@@ -14,12 +17,15 @@ while($true){
 
     try{
         $jevois_output  = $jevois_port.ReadLine()
-        $arduino_output = $arduino_port.ReadLine()
-        echo $jevois_output
-        echo $arduino_port
+		echo $jevois_output
         $arduino_port.write($jevois_output)
-        $jevois_port.write($arduino_port)
-        
+    } catch {
+       # Start-Sleep -m 50
+    }
+	
+	try{
+        $arduino_output = $arduino_port.ReadLine()    
+        echo $arduino_output
     } catch {
        # Start-Sleep -m 50
     }
@@ -30,10 +36,15 @@ while($true){
         if (($key.key -eq "Q")) {
             echo "[bridge] Quitting, user pressed Q..."
             break
-        } else if  (($key.key -eq " ")) {
+        } elseif  (($key.key -eq "g")) {
             echo "[bridge] Sending start command"
             $arduino_port.write("g")
+			$jevois_port.write("GRATERTTM")
         }
     }
     
 }
+
+
+$jevois_port.Close()
+$arduino_port.Close()
